eeeef75a229b00aee779bc189da12fbc86e50c45
netomedeirosdev <netomedeirosdev@gmail.com>
2023-12-07 20:13:52 -0300

    Create the project

---
+++ src/main/java/com/luizmedeirosn/homeads/HomeadsApplication.java
@@ -0,0 +1,22 @@
+package com.luizmedeirosn.homeads;
+
+import java.io.IOException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeadsApplication {
+
+	public static void main(String[] args) {
+		try {
+			new ProcessBuilder("bash", "-c", "clear").inheritIO().start().waitFor();
+		} catch (InterruptedException | IOException e) {
+			e.printStackTrace();
+			Thread.currentThread().interrupt();
+		}
+		
+		SpringApplication.run(HomeadsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizmedeirosn/homeads/HomeadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizmedeirosn.homeads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
